4ae6d18b9fb4d8a731a6d9873f75198705f4f365
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-21 12:35:04 -0300

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -3,72 +3,49 @@ import pandas as pd
 import os
 from time import sleep
 
-# Inicializando o estado da sessão para os campos do formulário
-def initialize_session_state():
-    if 'cidade' not in st.session_state:
-        st.session_state.cidade = ''
-    if 'nome' not in st.session_state:
-        st.session_state.nome = ''
-    if 'telefone' not in st.session_state:
-        st.session_state.telefone = ''
-    if 'rg' not in st.session_state:
-        st.session_state.rg = ''
-    if 'cpf' not in st.session_state:
-        st.session_state.cpf = ''
-    if 'endereco_obra' not in st.session_state:
-        st.session_state.endereco_obra = ''
-    if 'endereco_resid' not in st.session_state:
-        st.session_state.endereco_resid = ''
-    if 'obs' not in st.session_state:
-        st.session_state.obs = ''
-
-initialize_session_state()
-
 def are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
     return nome and telefone and rg and cpf and endereco_obra and endereco_resid
-
+    
 def cadastrar():
     if os.path.exists('clientes.csv'):
         tabclientes = pd.read_csv('clientes.csv', sep=",")
 
         with st.form("configp"):
-            st.session_state.cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
+            cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
                 "Condeúba",
                 "Maetinga",
                 "Pres. Jânio Quadros",
                 "Cordeiros",
                 "Piripá",
                 "Mortugaba"
-            ], index=0 if st.session_state.cidade == '' else ["Condeúba", "Maetinga", "Pres. Jânio Quadros", "Cordeiros", "Piripá", "Mortugaba"].index(st.session_state.cidade))
-            
-            st.session_state.nome = st.text_input("Digite seu nome", placeholder="Nome Completo", value=st.session_state.nome)
-            st.session_state.telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx", value=st.session_state.telefone)
-            st.session_state.rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx", value=st.session_state.rg)
-            st.session_state.cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx", value=st.session_state.cpf)
-            st.session_state.endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro", value=st.session_state.endereco_obra)
-            st.session_state.endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro", value=st.session_state.endereco_resid)
-            st.session_state.obs = st.text_area("Observação", placeholder="não obrigatório", value=st.session_state.obs)
-
+            ])
+            nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
+            telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
+            rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx")
+            cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx")
+            endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
+            endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro")
+            obs = st.text_area("Observação", placeholder="não obrigatório")
             # Mensagem de aviso
             warning_message = st.empty()
 
             # Desativando o botão se algum campo estiver vazio
-            if not are_fields_filled(st.session_state.nome, st.session_state.telefone, st.session_state.rg, st.session_state.cpf, st.session_state.endereco_obra, st.session_state.endereco_resid):
+            if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
                 warning_message.warning("Por favor, preencha todos os campos.")
             else:
                 warning_message.empty()
-
+                
             btn_cadastro = st.form_submit_button("Cadastrar Dados")
-
-            if btn_cadastro and are_fields_filled(st.session_state.nome, st.session_state.telefone, st.session_state.rg, st.session_state.cpf, st.session_state.endereco_obra, st.session_state.endereco_resid):
-                cidadec = st.session_state.cidade
-                cliente = st.session_state.nome
-                fone = st.session_state.telefone
-                rgc = st.session_state.rg
-                cpfc = st.session_state.cpf
-                end1 = st.session_state.endereco_obra
-                end2 = st.session_state.endereco_resid
-                obs1 = st.session_state.obs
+    
+            if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
+                cidadec = cidade
+                cliente = nome
+                fone = telefone
+                rgc = rg
+                cpfc = cpf
+                end1 = endereco_obra
+                end2 = endereco_resid
+                obs1 = obs
                 data2 = pd.DataFrame(tabclientes)
                 d = {"Cidade": cidadec, 
                      "Nome": cliente, 
@@ -77,64 +54,57 @@ def cadastrar():
                      "CPF": cpfc, 
                      "Endereço_Obra": end1,
                      "Endereço_Residencial": end2,
-                     "Observação": obs1
+                     "Observação":obs1
                     }
                 df2 = pd.DataFrame(d, index=[0])
                 data2 = pd.concat([data2, df2], ignore_index=True)
                 data2.to_csv('clientes.csv', index=False)
                 st.success("# Cadastro Efetuado com sucesso!!!!")
-                
-                # Redefinindo os campos do formulário
-                st.session_state.cidade = ''
-                st.session_state.nome = ''
-                st.session_state.telefone = ''
-                st.session_state.rg = ''
-                st.session_state.cpf = ''
-                st.session_state.endereco_obra = ''
-                st.session_state.endereco_resid = ''
-                st.session_state.obs = ''
                 sleep(3)
                 st.experimental_rerun()
+                
+
+
+
     else:
         with st.form("config"):
-            st.session_state.cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
+            cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
                 "Condeúba",
                 "Maetinga",
                 "Pres. Jânio Quadros",
                 "Cordeiros",
                 "Piripá",
                 "Mortugaba"
-            ], index=0 if st.session_state.cidade == '' else ["Condeúba", "Maetinga", "Pres. Jânio Quadros", "Cordeiros", "Piripá", "Mortugaba"].index(st.session_state.cidade))
-            
-            st.session_state.nome = st.text_input("Digite seu nome", placeholder="Nome Completo", value=st.session_state.nome)
-            st.session_state.telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx", value=st.session_state.telefone)
-            st.session_state.rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx", value=st.session_state.rg)
-            st.session_state.cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx", value=st.session_state.cpf)
-            st.session_state.endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro", value=st.session_state.endereco_obra)
-            st.session_state.endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro", value=st.session_state.endereco_resid)
-            st.session_state.obs = st.text_area("Observação", placeholder="não obrigatório", value=st.session_state.obs)
-
+            ])
+            nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
+            telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
+            rg = st.text_input("Digite seu RG",placeholder="xxxxxxxx-xx")
+            cpf = st.text_input("Digite seu CPF",placeholder="xxx.xxx.xxx-xx")
+            endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
+            endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro" )
+            obs = st.text_area("Observação", placeholder="não obrigatório")
             # Mensagem de aviso
             warning_message = st.empty()
 
             # Desativando o botão se algum campo estiver vazio
-            if not are_fields_filled(st.session_state.nome, st.session_state.telefone, st.session_state.rg, st.session_state.cpf, st.session_state.endereco_obra, st.session_state.endereco_resid):
+            if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
                 warning_message.warning("Por favor, preencha todos os campos.")
             else:
                 warning_message.empty()
-
-            btn_cadastro = st.form_submit_button("Cadastrar Dados")
-
-            if btn_cadastro and are_fields_filled(st.session_state.nome, st.session_state.telefone, st.session_state.rg, st.session_state.cpf, st.session_state.endereco_obra, st.session_state.endereco_resid):
-                cidadec = st.session_state.cidade
-                cliente = st.session_state.nome
-                fone = st.session_state.telefone
-                rgc = st.session_state.rg
-                cpfc = st.session_state.cpf
-                end1 = st.session_state.endereco_obra
-                end2 = st.session_state.endereco_resid
-                obs1 = st.session_state.obs
-
+                
+                btn_cadastro = st.form_submit_button("Cadastrar Dados")
+    
+            if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
+                
+                cidadec = cidade
+                cliente = nome
+                fone = telefone
+                rgc = rg
+                cpfc = cpf
+                end1 = endereco_obra
+                end2 = endereco_resid
+                obs1 = obs
+                
                 data2 = pd.DataFrame(columns=['Cidade', 
                                               'Nome', 
                                               'Telefone',
@@ -159,15 +129,7 @@ def cadastrar():
                 data2.to_csv('clientes.csv', index=False)
                 st.success("# Cadastro Efetuado com sucesso!!!!")
                 
-                # Redefinindo os campos do formulário
-                st.session_state.cidade = ''
-                st.session_state.nome = ''
-                st.session_state.telefone = ''
-                st.session_state.rg = ''
-                st.session_state.cpf = ''
-                st.session_state.endereco_obra = ''
-                st.session_state.endereco_resid = ''
-                st.session_state.obs = ''
+                
                 sleep(3)
                 st.experimental_rerun()
 
