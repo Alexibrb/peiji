c3f5eb0b24fcc88d6284d38d6f4316e954c141e9
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-21 12:16:34 -0300

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -165,3 +165,44 @@ def cadastrar():
                 
                 sleep(3)
                 st.experimental_rerun()
+
+
+
+def visualisar():
+    if os.path.exists('clientes.csv'):
+        tabclientes = pd.read_csv('clientes.csv', sep=",")
+        senha = st.text_input("Digite a senha para visualizar os dados", type="password")
+        btn_visualizar = st.button("Visualizar")
+        if btn_visualizar:
+            if senha == "ki47trqwe":
+
+                st.table(tabclientes)
+
+            else:
+                st.error("Senha errada")
+    else:
+        st.warning("### Nenhum Registro Encontrado")
+
+
+def apagar():
+    if os.path.exists('clientes.csv'):
+        senha2 = st.text_input("Digite a Senha para Apagar os dados", type="password")
+        btn_apagar = st.button("Apagar Dados dos Clientes", type="primary")
+        if btn_apagar:
+            if senha2 == "ki47trqwe":
+
+
+                os.remove('clientes.csv')
+                st.success("Dados Deletados com sucesso")
+                sleep(0.5)
+                st.experimental_rerun()
+            else:
+                st.error("Senha errada")
+
+
+
+
+
+
+
+
