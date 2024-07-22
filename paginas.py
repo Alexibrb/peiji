38e6a00c4abf0449a1c4b46ac8472cc039fee3e2
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-21 22:56:39 -0300

    Update paginas.py

--- paginas.py
+++ paginas.py
@@ -94,7 +94,7 @@ def cadastrar():
             else:
                 warning_message.empty()
                 
-                btn_cadastro = st.form_submit_button("Cadastrar Dados")
+            btn_cadastro = st.form_submit_button("Cadastrar Dados")
     
             if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
                 
